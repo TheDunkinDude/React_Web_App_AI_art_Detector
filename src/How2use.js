e9c7948539c59c3477977cfa18978ed2439c24ff
TheDunkinDude <tharrun.21bce8563@vitapstudent.ac.in>
2023-11-27 11:18:57 +0530

    first commit

---
+++ src/How2use.js
@@ -0,0 +1,19 @@
+import './How2Use.css'
+
+
+function HowBody(){
+
+    return (
+        <div className='MainBody'>
+            <div className='Grouper'>
+                <h1> Easy To Use </h1>
+                <p className='ContentE2U'>
+                Introducing our user-friendly AI Art Detector web app, designed to effortlessly elevate your art exploration experience. Whether you're an art enthusiast, collector, or just curious about the origins of a particular piece, our intuitive platform simplifies the process. Upload an image of the artwork, and let our advanced AI algorithms analyze its distinctive features. Within seconds, receive detailed insights into the artist, style, and historical context, enhancing your understanding and appreciation of the art before you. Embrace the future of art discovery with our accessible and seamless AI Art Detector
+                </p>
+            </div>
+
+        </div>
+    );
+}
+
+export default HowBody;
---
+++ src/Report.js
@@ -0,0 +1,57 @@
+import './Report.css'
+import { useState } from 'react';
+import axios from 'axios';
+
+function Report(){
+
+    const [section, setSection] = useState('Navigation');
+    const [complain, setComplain] = useState(null);
+
+    function SectionHandle(value){
+        setSection(value);
+    };
+
+    function ComplaintHandle(value){
+        setComplain(value);
+    };
+    
+    function FormSubmit(){
+            
+            const formdata = {section,complain};
+
+            axios.post('http://localhost:5000/api/submit', formdata)
+            .then(response => {
+              console.log(response.data);
+              setComplain(null);
+              setSection('Navigation');
+            })
+            .catch(error => {
+              console.error('Error submitting data:', error);
+            });
+
+
+    };
+
+    return(
+        <div className='MainBody'>
+            <div className='ContainReportForm'> 
+                <div > 
+                    <select onChange={e => SectionHandle(e.target.value)} value = {section} className='SelectorBar'>
+                        <option value="Navigation"> Navigation </option>
+                        <option value="Uploadiing"> Uploadiing an image </option>
+                        <option value="Results"> Results Area </option>
+                        <option value="Prediction"> Prediction </option>
+                    </select>
+                </div>
+                <div>
+                    <textarea onChange={e => ComplaintHandle(e.target.value)}  rows="10" cols="159" value={complain} className='ComplainSection'/>
+                </div>
+                <div>
+                    <button onClick={FormSubmit} className='SubmitBttn'> <b> Submit Form </b></button>
+                </div>
+            </div>
+    </div>
+    );
+}
+
+export default Report;
